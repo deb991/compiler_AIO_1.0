b0da02e66397d3f700231998b3784684fe4ef220
Ozzius <deb991@users.noreply.github.com>
2019-03-27 19:15:06 +0530

    Added all menu item
    Added all menu item.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,39 +1,110 @@
-#!/usr/bin/emv python -i
-import os
+#!/usr/bin/env python -i
+from tkinter import *
+from tkinter import scrolledtext, filedialog, Frame
+from commands import *
+from tkinter.messagebox import askokcancel
+import threading
 
-def new__file():
-    print('Create a new file as per the requirement')
-    #return 'EOF'
+root = Tk(className='just another code editor but with some innovative features')
+edtr = scrolledtext.ScrolledText(root, width=100, height=100)
+frame = Frame
 
+#================Few on demand commands=================#
+def exit():
+    if askokcancel("Quit", "Do you really want to quit?"):
+        root.destroy()
+
+t_exit = threading.Thread(target=exit)
+
+def open__file():
+    print('Open an existing file from the system.')
+    # return 'EOF'
+    file = filedialog.askopenfile(parent=root, mode='rb', title='Select a file')
+    if file != None:
+        contents = file.read()
+        edtr.insert('1.0', contents)
+        file.close()
+
+t = threading.Thread(target=open__file)
 
 def save__file():
     print('save a file')
-    #return 'EOF'
+    file = filedialog.asksaveasfile(mode='w')
+    if file != None:
+    # slice off the last character from get, as an extra return is added
+        data = edtr.get('1.0', END + '-1c')
+        file.write(data)
+        file.close()
+        # return 'EOF'
+
+t_save__file = threading.Thread(target=save__file)
+
+#==============Menu Bar======================#
+
+menubar = Menu(root)
+fileMenu = Menu(menubar)
+menubar.add_cascade(label='File', menu=fileMenu)
+
+fileMenu.add_command(label='New', command=new__file)
+fileMenu.add_command(label='Open', command=open__file)
+fileMenu.add_command(label='Save', command=save__file)
+fileMenu.add_command(label='Save as', command=save_as__file)
+fileMenu.add_command(label='Save All', command=save__all)
+fileMenu.add_command(label='Export to HTML', command=export__html)
+fileMenu.add_command(label='Make file read only', command=mkFleRdOnly)
+fileMenu.add_command(label='Exit', command=exit)
+fileMenu.add_separator()
+
+editMenu = Menu(menubar)
+menubar.add_cascade(label='Edit', menu=editMenu)
+
+editMenu.add_command(label='Cut', command=cut)
+editMenu.add_command(label='Copy', command=copy)
+editMenu.add_command(label='ClipBoard', command=clpBrd)
+editMenu.add_command(label='Paste', command=paste)
+editMenu.add_separator()
+editMenu.add_command(label='Delete', command=delt)
 
 
-def save_as__file():
-    print('Save as option for existing file')
-    #return 'EOF'
+viewMenu = Menu(menubar)
+menubar.add_cascade(label='View', menu=viewMenu)
+
+viewMenu.add_command(label='Full Screen mode', command=FSM)
+viewMenu.add_command(label='Presentation mode', command=PsM)
+viewMenu.add_separator()
+
+runMenu = Menu(menubar)
+menubar.add_cascade(label='Run', menu=runMenu)
+
+runMenu.add_command(label='Run', command=run)
+runMenu.add_command(label='Debug', command=dbug)
+runMenu.add_separator()
+runMenu.add_command(label='View Break points', command=VBP)
 
 
-def save__all():
-    print('save all files under an existing folder')
-    #return 'EOF'
+sett = Menu(menubar)
+menubar.add_cascade(label='Settings', menu=sett)
 
+sett.add_command(label='Settings', command=sett)
+sett.add_separator()
+sett.add_command(label='Project Setting', command=sett__P)
 
-def export__html():
-    print('Export to HTML')
-    #return 'EOF'
+help = Menu(menubar)
+menubar.add_cascade(label='Help', menu=help)
 
+help.add_command(label='About', command=abt)
+help.add_command(label='File Manager', command=fleAnlzer)
 
-def mkFleRdOnly():
-    print('Make file read only mode')
-    #return 'EOF'
+#==========================Menu Bar==================#
 
-def cut():
-    print('cut')
-    #return 'EOF'
+#======================Thread========================#
+thread_list = [t, t_exit, t_new__file, t_new__file, t_save_as__file, t_save__all, t_export__html,
+               t_cut, t_copy, t_clpBrd]
 
-def check__code():
-    print('this method is only for on demand issues')
-    #return 'EOF'
+#======================Thread========================#
+
+root.config(menu=menubar)
+edtr.pack()
+
+if __name__ == '__main__':
+    root.mainloop()
