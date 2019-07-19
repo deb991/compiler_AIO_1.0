fb227bf919a84cb00756d753594637ec832b4d11
Ozzius <deb991@users.noreply.github.com>
2019-07-19 20:51:33 +0530

    Script updated.
    Script updated.

--- test/test.py
+++ test/test.py
@@ -9,23 +9,26 @@ root = Tk()
 #root.configure()
 #progressBar.progressBar(root)
 
-tabcontrol = ttk.Notebook(root)
-tab1 = ttk.Frame(tabcontrol)
-tabcontrol.add(tab1)
-tabcontrol.pack(expand=1, fill="both")
+notebook = ttk.Notebook(root)
+tab1 = ttk.Frame(notebook)
+notebook.add(tab1)
+#notebook.pack(expand=1, fill="both")
+notebook.grid(row=1, column=0, columnspan=50, rowspan=49, sticky='NESW')
 
-#frame = tk.Frame(root, bd=4, bg="grey")
-#frame.place(x=10, y=10)
+tab2 = ttk.Frame(notebook)
+notebook.add(tab2)
+notebook.grid(row=1, column=0, columnspan=50, rowspan=49, sticky='NESW')
+
 make_draggable(tab1)
 
 notes = tk.Text(tab1)
 notes.pack(ipadx=5)
 
-L_Side_menubar = tk.Frame(root, background="blue")
-L_Side_menubar.pack(fill='both', side="bottom")
+#L_Side_menubar = tk.Frame(root, background="blue")
+#L_Side_menubar.pack(fill='both', side="bottom")
 
 
-progressBar.progressBar(L_Side_menubar)
+#progressBar.progressBar(root)
 
 #Here another switch required to close the Progree bar after loaded fully.
 #Another switch required to pin down the moveable widget.
