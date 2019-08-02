1f070d375b8a7a4a1500729f3927b4006d9328db
Ozzius <deb991@users.noreply.github.com>
2019-08-02 21:01:23 +0530

    Code updated.
    Code updated.

--- kernel/parser/CodeStructure/__init__.py
+++ kernel/parser/CodeStructure/__init__.py
@@ -14,21 +14,26 @@ fileList = []
 module_list = []
 
 statements = input('Enter the file path details to review : ')
-with open (statements, 'r') as d:
-    data = d.read()
-    lines = data.splitlines()
-    words = data.split()
-    print('Total line number of the code :: ', len(lines))
-    #print(words)
-    for i in words:
-        # print(i)
-        fileList.append(i)
 
-for item in keyWords:
-    if item in fileList:
-        print('Found Match :: ', item, '\t >> Line Number:: ', enumerate(data))
-    else:
-        print('Not Found!!!')
+def codeChecking():
+    with open(statements, 'r') as d:
+        data = d.read()
+        lines = data.splitlines()
+        words = data.split()
+        print('Total line number of the code :: ', len(lines))
+        # print(words)
+        for i in words:
+            # print(i)
+            fileList.append(i)
+
+    for item in keyWords:
+        if item in fileList:
+            print('Found Match :: ', item, '\t >> Line Number:: ', enumerate(data))
+        else:
+            print('Not Found!!!')
+
+pattern = input('Enter file types to be search\t::\t')
+path = input('Enter the path, where need to check\t ::\t')
 
 def find(pattern, path):
     result = []
@@ -36,26 +41,9 @@ def find(pattern, path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
-        return result
+    print(result)
 
-##Root window defined:
-#root = tk.Tk()
-#root.title('Debug Window :: ')
-#
-##canvas defined:
-#frame = Frame(root, width=1200, height=600)
-#
-#np1 = tk.Text(frame)
-#np1.pack(side="right", fill="both", expand=True)
-#vScBar1 = tk.Scrollbar(orient="vertical", command=frame)
-#np1.configure(yscrollcommand=vScBar1)
-#vScBar1.config(command=np1.yview)
-#np1.config(yscrollcommand=vScBar1.set, background='black', foreground='yellow')
-#vScBar1.pack(side="right", fill="y")
-#
-#np1.insert(END, data)
-#
-##np2.insert(END, data)
-#
-#if __name__ == '__main__':
-#    root.mainloop()
+
+codeChecking()
+find(pattern, path)
+
